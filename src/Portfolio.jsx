528d99b1d4422ce17fc4f4852b3703f6244507ac
gabriel sanchez <gabrielpelota.8@gmail.com>
2022-07-14 22:40:02 -0500

    button repair

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -24,7 +24,7 @@ const navigate = useNavigate()
             <li><button onClick={()=>navigate('./contact')}>Contacto</button> </li>
         </ul>
         <span className='btn-menu'>
-          <i class="fa-solid fa-bars-staggered"></i>
+          <i className='fa-solid fa-bars-staggered'></i>
         </span>
     </nav>
   
